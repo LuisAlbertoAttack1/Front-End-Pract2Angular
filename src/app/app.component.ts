ce0cb9c8d3eb512f1d5f5a00e409728ce05ecf46
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-03-23 12:30:10 -0600

    NuevoProyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practica2';
+  public titulo : string = "contador";
+  public numero : number = 10;
+  public valor : number = 5;
+  public sumar():void{
+    this.numero += 1;
+  }
+  public restar():void{
+    this.numero -= 1;
+  }
+
+  public contador(valor : number):void{
+    this.numero += valor;
+  }
 }
